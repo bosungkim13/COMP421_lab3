239af0914b8d3ff8c9868e899e9db88146146ca2
bosungkim13 <89793411+bosungkim13@users.noreply.github.com>
2023-04-11 21:39:49 -0500

    local copy

---
+++ cache.h
@@ -0,0 +1,33 @@
+#include <stdbool.h>
+#include "hashTable.h"
+
+typedef struct CacheNode {
+    int key;
+    void* value;
+    struct CacheNode* prev;
+    struct CacheNode* next;
+    bool dirty;
+} CacheNode;
+
+typedef struct Cache {
+    CacheNode* head;
+    CacheNode* tail;
+    int capacity;
+    int len;
+    HashTable* table;
+} Cache;
+
+Cache* InitCache(int capacity);
+
+CacheNode* InitCacheNode(int key, void* val);
+
+CacheNode* PutItemInCache(Cache* cache, int key, void* value);
+
+void* GetItemFromCache(Cache* cache, int key);
+
+void SetHead(Cache* cache, CacheNode* node);
+
+void RemoveNode(Cache* cache, CacheNode* node);
+
+void SetDirty(Cache* cache, int key);
+
